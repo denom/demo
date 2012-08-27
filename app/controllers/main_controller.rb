1dbebbf6d5b557cc05a4600242ea047bb255f477
Dimitrios Kyriazopoulos <jim.kyriazopoulos@nocturnart.com>
2012-08-27 13:50:43 +0300

    initial commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+  <h1> Welcome to the Rails installer demo </h1>
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '0de28aa1f0703d6ef083b39df466374ff8130c02e7b76fc1fc23e390f6d76855c854a663b4daf035a65de590825fb29114c79a766ce22709cab422472837d0b4'
